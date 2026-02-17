9cbdd8970fe4b8fef6bcd9a73284d833f6a6bf21
Camilo Alejandro <79614560+its-camilo@users.noreply.github.com>
2026-02-17 19:01:25 +0000

    feat: update HomePage component and integrate into app structure

--- ProyectoFinal/src/app/components/pages/home-page/home-page.ts
+++ ProyectoFinal/src/app/components/pages/home-page/home-page.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home-page',
+  standalone: true,
   imports: [],
   templateUrl: './home-page.html',
-  styleUrl: './home-page.scss',
+  styleUrls: ['./home-page.scss'],
 })
 export class HomePage {
 
