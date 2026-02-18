2c8080d93ad9fa3bbf7a738dbff6e5eb52a9627f
Camilo Alejandro <79614560+its-camilo@users.noreply.github.com>
2026-02-18 22:05:40 +0000

    feat: agregar funcionalidad de carga y estilos a la lista básica

--- ProyectoFinal/src/app/modules/lists/lista-basica/lista-basica.ts
+++ ProyectoFinal/src/app/modules/lists/lista-basica/lista-basica.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-lista-basica',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './lista-basica.html',
   styleUrl: './lista-basica.scss',
 })
-export class ListaBasica {
+export class ListaBasica implements OnInit {
 
+  listaElementos: string[] = ['Tarea 1', 'Tarea 2', 'Tarea 3'];
+
+  cargando: boolean = false;
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  cambiarCargando() {
+    this.cargando = !this.cargando;
+  }
 }
